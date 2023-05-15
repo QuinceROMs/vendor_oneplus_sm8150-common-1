8ba1763c2fc9ededa52ac0ed7cf88b32cd20d11c
Pranav Vashi <neobuddy89@gmail.com>
2023-05-15 09:57:52 +0300

    sm8150-common: Add 786 Adreno driver from @Lynchp1n
    Change-Id: Id12729049d0966db64b00b78850ca32de9332f03

--- sm8150-common-vendor.mk
+++ sm8150-common-vendor.mk
@@ -266,6 +266,7 @@ PRODUCT_PACKAGES += \
     libEGL_adreno \
     libGLESv1_CM_adreno \
     libGLESv2_adreno \
+    libVkLayer_ADRENO_qprofiler \
     libq3dtools_adreno \
     libq3dtools_esx \
     android.hardware.bluetooth@1.0-impl-qti \
@@ -278,6 +279,7 @@ PRODUCT_PACKAGES += \
     libC2D2 \
     libCB \
     libOpenCL \
+    libOpenCL_adreno \
     libVkLayer_q3dtools \
     libadreno_utils \
     libadsprpc \
@@ -296,8 +298,11 @@ PRODUCT_PACKAGES += \
     libgpudataproducer \
     libgsl \
     libidl \
+    libkcl \
+    libkernelmanager \
     libllvm-glnext \
     libllvm-qcom \
+    libllvm-qgl \
     libmdmdetect \
     libmdsprpc \
     libmmosal_vendor \
