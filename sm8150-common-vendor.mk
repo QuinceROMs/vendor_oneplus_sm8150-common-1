5e040b03302d870912800950f5bf9962462f9c88
Michael Bestas <mkbestas@lineageos.org>
2026-06-22 03:46:06 +0300

    sm8150-common: Update WFD system blobs from marvel_g A171WEH.20
    Change-Id: I733c2bf342175474ce7f02d467a17ded6f0a8760

--- sm8150-common-vendor.mk
+++ sm8150-common-vendor.mk
@@ -898,7 +898,7 @@ PRODUCT_PACKAGES += \
     prebuilt_vendor.oplus.hardware.camera.slogan@1.0_sysext \
     vendor.qti.hardware.dpmservice@1.0 \
     vendor.qti.hardware.wifidisplaysession@1.0 \
-    vendor.qti.hardware.wifidisplaysession_aidl-V1-ndk \
+    vendor.qti.hardware.wifidisplaysession_aidl-V2-ndk \
     vendor.qti.imsrtpservice@3.0 \
     vendor.oplus.hardware.orms@1.0 \
     liba2dpoffload_odm \
